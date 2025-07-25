1ab4adf7af18ad04dfb30f64c53cd6fae987b4b5
komola-me <komolaziya28@gmail.com>
2025-07-25 19:56:21 +0500

    fix: some changes

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR, 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
